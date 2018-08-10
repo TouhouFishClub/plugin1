3f77f7bce6c0a748aba519720674cd9184c64ab6
Flandre <flantan233@gmail.com>
2018-08-10 15:47:35 +0800

    update theme

--- js/run.js
+++ js/run.js
@@ -1,26 +1,20 @@
-function run(){
-  console.log(123);
-}
-
-function bililive(){
-  document.body.innerHTML=document.getElementsByClassName('player-section p-relative border-box none-select z-player-section')[0].innerHTML
-}
-
-function table(){
-  var tbhtml = '<table>';
-  tbhtml = tbhtml + '<tr><td><iframe id="a1" src="https://live.bilibili.com/39277"></iframe></td><td><iframe id="a2" src="https://live.bilibili.com/39277"></iframe></td></tr>';
-  tbhtml = tbhtml + '<tr><td><iframe id="a3" src="https://live.bilibili.com/39277"></iframe></td><td><iframe id="a4" src="https://live.bilibili.com/39277"></iframe></td></tr>';
-  tbhtml = tbhtml + '</table>';
-  document.body.innerHTML = tbhtml;
-}
-
-
 function simpleInnerHtml(id){
   document.getElementById(id).contentWindow.document.body.innerHTML=document.getElementById(id).contentWindow.document.getElementsByClassName('player-section p-relative border-box none-select z-player-section')[0].innerHTML
 }
 
-table();
-document.getElementById("a1").onload=function(){simpleInnerHtml("a1");}
-document.getElementById("a2").onload=function(){simpleInnerHtml("a2");}
-document.getElementById("a3").onload=function(){simpleInnerHtml("a3");}
-document.getElementById("a4").onload=function(){simpleInnerHtml("a4");}
+const showBiliRomes = (...args) => {
+  let blHtml = `<div class="flex-container ${(args.length == 3 || args.length == 4) ? 'flex-fix' : ''}">`
+  args.forEach((id, index) => {
+    blHtml += `<iframe class="bili_frame_item" id="bili_frame_${index}" src="https://live.bilibili.com/${id}" frameborder="0"></iframe>`
+  })
+  blHtml += '</div>'
+  document.body.innerHTML = blHtml
+}
+
+showBiliRomes(39277, 39277, 39277, 39277, 39277, 39277, 39277)
+
+Array.prototype.forEach.call(document.querySelectorAll('iframe'), ele => {
+  ele.onload = function(){
+    simpleInnerHtml(ele.id)
+  }
+})
