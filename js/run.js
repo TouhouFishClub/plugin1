74aac47f3106d74f473a71f4ed4ba6f4f0afb194
ter <a26214311@gmail.com>
2018-08-10 16:54:06 +0800

    fix

--- js/run.js
+++ js/run.js
@@ -1,24 +1,48 @@
 function simpleInnerHtml(id){
   setTimeout(function(){
     document.getElementById(id).contentWindow.document.querySelector('.bilibili-live-player-video-controller-web-fullscreen-btn > button').click()
-    document.getElementById(id).contentWindow.document.querySelector('aside-area-toggle-btn').click()
-  },5000)
-
+    setTimeout(function(){
+      document.getElementById(id).contentWindow.document.querySelector('#aside-area-toggle-btn').click()
+      console.log('22222222');
+    },1000);
+    console.log('1111111')
+  },3000)
 }
 
 const showBiliRomes = (...args) => {
   let blHtml = `<div class="flex-container ${(args.length == 3 || args.length == 4) ? 'flex-fix' : ''}">`
   args.forEach((id, index) => {
-    blHtml += `<iframe class="bili_frame_item" id="bili_frame_${index}" src="https://live.bilibili.com/${id}" frameborder="0"></iframe>`
+    blHtml += `<iframe class="bili_frame_item" id="bili_frame_${index}" src="https://live.bilibili.com/${id}" frameborder="1"></iframe>`
   })
   blHtml += '</div>'
   document.body.innerHTML = blHtml
 }
 
-showBiliRomes(245, 245)
 
-Array.prototype.forEach.call(document.querySelectorAll('iframe'), ele => {
-  ele.onload = function(){
-    simpleInnerHtml(ele.id)
-  }
-})
+
+
+
+
+
+function run(){
+  var idlist = getQueryString("id");
+  var idarr = idlist.split(",");
+  showBiliRomes(...idarr);
+  Array.prototype.forEach.call(document.querySelectorAll('iframe'), ele => {
+    ele.onload=function(){
+      simpleInnerHtml(ele.id)
+    }
+  })
+}
+
+
+
+
+function getQueryString(name)
+{
+  var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if(r!=null)return  unescape(r[2]); return null;
+}
+
+run();
