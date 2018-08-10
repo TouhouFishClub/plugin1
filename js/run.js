9519270e35c04da9e59e081bfa83e9c85a9405e3
ter <a26214311@gmail.com>
2018-08-10 14:01:00 +0800

    f

--- js/run.js
+++ js/run.js
@@ -6,4 +6,12 @@ function bililive(){
   document.body.innerHTML=document.getElementsByClassName('player-section p-relative border-box none-select z-player-section')[0].innerHTML
 }
 
-bililive();
+function table(){
+  var tbhtml = '<table>';
+  tbhtml = tbhtml + '<tr><td><iframe src="https://live.bilibili.com/39277"></iframe></td><td><iframe src="https://live.bilibili.com/39277"></iframe></td></tr>';
+  tbhtml = tbhtml + '<tr><td><iframe src="https://live.bilibili.com/39277"></iframe></td><td><iframe src="https://live.bilibili.com/39277"></iframe></td></tr>';
+  tbhtml = tbhtml + '</table>';
+  document.body.innerHTML = tbhtml;
+}
+
+table();
