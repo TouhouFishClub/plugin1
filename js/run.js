1b1f955df9031d4eb7abd9c68537fde5e853be4a
ter <a26214311@gmail.com>
2018-08-10 13:42:36 +0800

    f

--- js/run.js
+++ js/run.js
@@ -2,4 +2,8 @@ function run(){
   console.log(123);
 }
 
-run();
+function bililive(){
+  document.body.innerHTML=document.getElementsByClassName('player-section p-relative border-box none-select z-player-section')[0].innerHTML
+}
+
+bililive();
